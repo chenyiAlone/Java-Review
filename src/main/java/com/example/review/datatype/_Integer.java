ba8384d1e5f794487ff8309a98b4842e20107815
chenyiAlone <1193642781@qq.com>
2019-07-02 21:16:33 +0800

    Initial commit

---
+++ src/main/java/com/example/review/datatype/_Integer.java
@@ -0,0 +1,20 @@
+package com.example.review.datatype;
+/**
+ * ClassName: _Integer.java
+ * Author: chenyiAlone
+ * Create Time: 2019/7/2 15:02
+ * Description: 测试 Integer 类型
+ * 结论:
+ *
+ *
+ *
+ */
+public class _Integer {
+    public static void main(String[] args) {
+        int a = 1;
+        long b = 3000000000l;
+        Integer integer = 1;
+        int i = integer;
+        System.out.println(i);
+    }
+}
