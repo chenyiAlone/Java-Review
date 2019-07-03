4cc26bc982bd62a01024b8362a68ffad13193273
chenyiAlone <1193642781@qq.com>
2019-07-03 21:03:51 +0800

    review integer

--- src/main/java/com/example/review/datatype/_Integer.java
+++ src/main/java/com/example/review/datatype/_Integer.java
@@ -5,16 +5,27 @@ package com.example.review.datatype;
  * Create Time: 2019/7/2 15:02
  * Description: 测试 Integer 类型
  * 结论:
+ *  1.  自动装箱： Integer valueOf(int i)
+ *      自动拆箱： int intValue()
  *
- *
+ *  2.  IntegerCache.low ~ Integer.high 享元模式
+ *  3.  Integer.valueOf(int i) 新建对象
+ *  4.  long a = 123L;
+ *      float f = 234f;
  *
  */
 public class _Integer {
-    public static void main(String[] args) {
+    private static void init() {
         int a = 1;
-        long b = 3000000000l;
+        long b = 3000000000L;
         Integer integer = 1;
         int i = integer;
         System.out.println(i);
+    }
+
+
+
+    public static void main(String[] args) {
+        init(); // 测试初始化
     }
 }
