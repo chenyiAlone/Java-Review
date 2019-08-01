3da729defcbc6a5a62c242580d13d54918a67020
chenyiAlone <1193642781@qq.com>
2019-08-01 23:39:33 +0800

    泛型捕获转化

---
+++ src/main/java/com/example/review/polymorphic/generic/Catch.java
@@ -0,0 +1,47 @@
+package com.example.review.polymorphic.generic;
+
+import com.example.review.polymorphic.generic.democlass.Holder;
+import com.example.review.polymorphic.generic.democlass.Sub;
+import com.example.review.polymorphic.generic.democlass.Super;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.util.Utils.log;
+
+/**
+ * ClassName: Catch.java
+ * Author: chenyiAlone
+ * Create Time: 2019/7/27 22:39
+ * Description: 测试泛型捕获
+ */
+public class Catch {
+
+
+    private static <T> void f1(Holder<T> holder) {
+        T t = holder.get();
+        log("T: " + t);
+    }
+
+    private static void f2(Holder<?> holder) {
+        f1(holder);
+    }
+
+
+    private static void genericCatch() {
+        Holder holder = new Holder<Sub>(new Sub());
+        f2(holder);
+        f1(holder);  // Warning
+
+        /* Output:
+
+            T: Sub{}
+        *///:~
+    }
+
+
+    public static void main(String[] args) {
+        genericCatch();
+    }
+
+}
