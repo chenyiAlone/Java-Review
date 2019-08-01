b9a7d49cb2ee4c51c0d31e9c1a635c040fbc0cbe
chenyiAlone <1193642781@qq.com>
2019-08-01 23:40:34 +0800

    协变参数类型

---
+++ src/main/java/com/example/review/polymorphic/generic/CovariationAndInversion.java
@@ -0,0 +1,54 @@
+package com.example.review.polymorphic.generic;
+
+import com.example.review.polymorphic.generic.democlass.Sub;
+import com.example.review.polymorphic.generic.democlass.Super;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ClassName: CovariationAndInversion.java
+ * Author: chenyiAlone
+ * Create Time: 2019/7/27 18:00
+ * Description: 测试协变和逆变
+ *
+ *
+ */
+public class CovariationAndInversion {
+
+    private static Super parameterCovariant(Super su) { return new Sub(); }
+
+    /*
+    测试 协变参数类型
+    */
+    private static void parameterCovariantTest() {
+        parameterCovariant(new Sub());
+        parameterCovariant(new Super());
+    }
+
+    /*
+    测试 协变返回类型
+        导出类(子类)重写的父类方法 的 返回类型 可以是父类方法的 导出类型(子类)
+    */
+    private static void returnParameterCovariant() {
+        /* Output:
+
+            Super: Super get()
+            Sub extends Super: Sub get()
+        *///:~
+    }
+
+    /*
+    测试 数组 和 泛型 是否支持协变
+        1. 数组支持
+        2. 泛型不支持
+    */
+    private static void covariant() {
+        Number[] arr = new Integer[10]; // 数组支持协变
+        arr[1] = Integer.valueOf(1);
+
+        /** 泛型不支持协变 */
+//!        List<Number> list = new ArrayList<Integer>();    // Error
+    }
+
+}
