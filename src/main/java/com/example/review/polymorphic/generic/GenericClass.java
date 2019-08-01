4dd9b3bf6ef8884e732837e1240517bada5a7ad2
chenyiAlone <1193642781@qq.com>
2019-08-01 23:42:21 +0800

    泛型类

--- src/main/java/com/example/review/polymorphic/generic/GenericClass.java
+++ src/main/java/com/example/review/polymorphic/generic/GenericClass.java
@@ -7,22 +7,35 @@ import java.util.List;
  * ClassName: GenericClass.java
  * Author: chenyiAlone
  * Create Time: 2019/7/16 17:36
- * Description: 测试泛型类
+ * Description: 测试泛型类的性质
  */
-public class GenericClass {
+public class GenericClass<T> {
 
-    private static <T> List<T> getList() {
+    T value;
+
+    public GenericClass(T t) {
+        this.value = t;
+    }
+
+    /* com.example.review.polymorphic.generic.GenericClass.this' cannot be referenced from a static context! */
+//!    private static void staticMethod(T t) {} // Error
+
+    private List<T> getList() {
         return new ArrayList<T>();
     }
-    /*
-    测试类型推断
-    */
-    private static void genericDeduce() {
-        List<Integer> list = getList();
-        list.add(Integer.valueOf(12));
-    }
 
-    public static void main(String[] args) {
-        genericDeduce();
+
+    class TwoTuple<A, B> {}
+
+    class ThreeTuple<D, E, F> extends TwoTuple<D, E> {
+        D d;
+        E e;
+        F f;
     }
+    class ThreeTuple2<D, E, F> extends TwoTuple {}
+
+    class FourTuple1<D, F> extends ThreeTuple<Integer, String, String> {}
+    class FourTuple2<D> extends ThreeTuple<D, String, String> {}
+
+
 }
