662731f1f0fdec961a9e510c4e1692751a667799
chenyiAlone <1193642781@qq.com>
2019-08-01 23:41:33 +0800

    泛型擦除

--- src/main/java/com/example/review/polymorphic/generic/Erase.java
+++ src/main/java/com/example/review/polymorphic/generic/Erase.java
@@ -1,4 +1,11 @@
 package com.example.review.polymorphic.generic;
+
+import com.example.review.polymorphic.generic.democlass.Super;
+
+import java.util.ArrayList;
+
+import static com.example.util.Utils.log;
+
 /**
  * ClassName: Erase.java
  * Author: chenyiAlone
@@ -9,11 +16,32 @@ package com.example.review.polymorphic.generic;
 public class Erase {
     /*
     测试泛型擦除的影响
+        1. 不能直接使用泛型参数的类型信息，在编译期泛型会被擦除
+        2. 由于泛型擦除，运行中的 ArrayList<String> 和 ArrayInteger<Integer> 其实是一样的，
+           擦除道一个边界，都是 ArrayList<Object>
+
+
     */
-    private <T> void test() {
-//!       T t = new T();
+    private static <T> void testGenericErase() {
 //!       if (T instanceof Object); // Error
-       T t = (T)new Object();   // Uncheck warning
+//!       T t = new T();    // Error
+//!       T[] ts = new T[10];  // Error
+        T t = (T)new Object();   // Uncheck warning
+        T[] ts = (T[])new Object(); // Uncheck warning
+
+
+        Class clazz1 = new ArrayList<Integer>().getClass();
+        Class clazz2 = new ArrayList<String>().getClass();
+        log("clazz1 == clazz2: " + (clazz1 == clazz2));
+
+        /* Output:
+
+            clazz1 == clazz2: true
+        *///:~
+    }
+
+    public static void main(String[] args) {
+        testGenericErase();
     }
 
 }
